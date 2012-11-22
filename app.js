1d632ad02d70f3eff86539dbf10e11c6293a1c97
Stephen Woods <saw@stephenwoods.net>
2012-11-21 22:28:04 -0800

    better bird data please

--- app.js
+++ app.js
@@ -5,13 +5,15 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , hbs = require('express-hbs')
   , http = require('http');
 
 var app = express();
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.engine('hbs', hbs.express3({defaultLayout:__dirname + '/views/layout.hbs', partialsDir: __dirname + '/views/partials'}));
+  app.set('view engine', 'hbs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.static(__dirname + '/public'));
@@ -27,6 +29,6 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/api/:bird', routes.bird_api);
 
-http.createServer(app).listen(3000);
+http.createServer(app).listen(1337);
 
 console.log("Express server listening on port 3000");
