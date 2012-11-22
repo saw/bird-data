2d054ed2017641efe23c388cbd586c10b0a69ac7
Stephen Woods <saw@stephenwoods.net>
2012-11-21 23:25:53 -0800

    views, routes, models, basic data working without images

--- models/wiki.js
+++ models/wiki.js
@@ -6,6 +6,13 @@ function getArticle(name) {
     var promise = new Promise();
 
     if(cache[name]) {
+        if(cache[name] == 'fail'){
+            process.nextTick(function(){
+                promise.reject('404');
+            });
+            
+            return promise;
+        }
         var out = cache[name];
         process.nextTick(function(){
             promise.resolve(out);
@@ -16,16 +23,25 @@ function getArticle(name) {
           "http://en.wikipedia.org/wiki/" + name,
           ["http://code.jquery.com/jquery.js"],
           function (errors, window) {
-
+              if(errors) {
+                  console.log('errors', errors)
+              }
               var out = '';
               var text = window.$('#mw-content-text p').each(function(index, el){
                  out += window.$(el).html().replace(/\"\/wiki\//g, '"http://en.wikipedia.org/wiki/') + '\n'; 
               });
+              
+              if(out == '') {
+                  promise.reject('No content');
+                  cache[name] = 'fail';
+                  return;
+              }
 
               var outData = {
                   name:name.replace(/_/g, ' '),
                   content:out
               };
+              
               promise.resolve(outData);
               cache[name] = outData;
           }
--- routes/bird-route.js
+++ routes/bird-route.js
@@ -1,7 +1,21 @@
 var app;
+var wiki = require('../models/wiki.js');
 
 function birdPage(req, res, next) {
-    res.render('bird', {name: req.params.name});
+    
+    wiki.getArticle(req.params.name).then(
+        function(resp) {
+            
+            resp.content = '<p>' + resp.content + '</p>';
+            resp.content = resp.content.replace(/\n/g, '</p><p>');
+            
+            res.render('bird', resp);
+        },
+        function(err) {
+            res.end('error');
+        }
+    );
+    
 }
 
 module.exports = function(thisapp) {
