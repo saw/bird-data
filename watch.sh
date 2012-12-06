f16c13d8115c1808168c72cba3811fd4f4b768ca
Stephen Woods <saw@stephenwoods.net>
2012-12-05 21:01:51 -0800

    who will watch the watchers?

--- watch.sh
+++ watch.sh
@@ -1,1 +1,3 @@
-handlebar-rider -w -i views -o public/javascripts/templates.js
+handlebar-rider -w -i views -o public/javascripts/templates.js  > watchlog 2>&1 &
+compass watch public/stylesheets/ > watchlog 2>&1 &
+tail -f watchlog
